7bbf56ef6c06718cf5af0ece823753e60ee633a4
Filipe de Paula <164094327+Fsp30@users.noreply.github.com>
2026-03-31 11:50:10 -0300

    Add files via upload

---
+++ ContaBancaria.java
@@ -0,0 +1,31 @@
+public class ContaBancaria {
+    private String titular;
+    private int numeroConta;
+    private float saldo;
+
+    public void setTitular(String titular) {
+        if()
+        this.titular = titular;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+}
