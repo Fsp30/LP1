f00730f51b6534a4d9c201c3469c2317200daf7c
Filipe de Paula <164094327+Fsp30@users.noreply.github.com>
2026-04-28 10:39:22 -0300

    Delete lista_2_heranca/pagamento directory

--- lista_2_heranca/pagamento/Funcionario.java
+++
@@ -1,6 +0,0 @@
-package lista_2_heranca.pagamento;
-
-public abstract class Funcionario {
-    float salarioBase = 1000.0f;
-    public abstract float calcularPagamento();
-}
--- lista_2_heranca/pagamento/FuncionarioComum.java
+++
@@ -1,9 +0,0 @@
-package lista_2_heranca.pagamento;
-
-public class FuncionarioComum extends Funcionario{
-    private Funcionario funcionarioComum;
-
-    public float calcularPagamento() {
-        return funcionarioComum.salarioBase;
-    }
-}
